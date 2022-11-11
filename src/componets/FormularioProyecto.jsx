3297bec79532da4cfaa468ad751b654637b48222
Johan <johanoscarmarte@gmail.com>
2022-11-10 21:23:34 -0400

    Correcion de datos en el formulario

--- src/componets/FormularioProyecto.jsx
+++ src/componets/FormularioProyecto.jsx
@@ -1,17 +1,47 @@
 import { useState } from "react"
+import useProyectos from "../hooks/useProyectos"
+import Alerta from "./Alerta"
 
 const FormularioProyecto = () => {
     const [nombre, setNombre] = useState('')
     const [descripcion, setDescripcion] = useState('')
     const [cliente, setCliente] = useState('')
     const [fechaEntrega, setFechaEntrega] = useState()
+
+  
+    const { mostrarAlerta, alerta, submitProyecto } = useProyectos();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if ([nombre, descripcion, fechaEntrega, cliente].includes('')) {
+            mostrarAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+
+            })
+
+            return
+            
+        }
+        submitProyecto({nombre, descripcion, fechaEntrega, cliente})
+
+    }
+
+    const { msg } = alerta;
     
 
 
 
     
   return (
-      <form className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow ">
+      <form
+          className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow "
+          onSubmit={handleSubmit}
+      
+      >
+          {msg && <Alerta alerta={alerta}/>}
+          
           <div className="mt-5">
           <label
               className="text-gray-700 uppercase font-bold text-sm"
@@ -23,7 +53,7 @@ const FormularioProyecto = () => {
               className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
               placeholder="Nombre del Proyecto"
               value={nombre}
-              onChange={e =>setNombre(e.target.e)}
+              onChange={e =>setNombre(e.target.value)}
           />
           </div>
 
@@ -38,7 +68,7 @@ const FormularioProyecto = () => {
               className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
               placeholder="Descripcion del proyecto"
               value={descripcion}
-              onChange={e =>setDescripcion(e.target.e)}
+              onChange={e =>setDescripcion(e.target.value)}
           />
           </div>
 
@@ -53,7 +83,7 @@ const FormularioProyecto = () => {
               type="date"
               className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
               value={fechaEntrega}
-              onChange={e =>setFechaEntrega(e.target.e)}
+              onChange={e =>setFechaEntrega(e.target.value)}
           />
           </div>
 
@@ -69,7 +99,7 @@ const FormularioProyecto = () => {
               className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
               placeholder="Nombre del Cliente"
               value={cliente}
-              onChange={e =>setCliente(e.target.e)}
+              onChange={e =>setCliente(e.target.value)}
           />
           </div>
           <input
